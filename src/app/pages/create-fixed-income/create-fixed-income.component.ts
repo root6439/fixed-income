6e530c6e7f98463dbe46bc5294a0a76b693060c1
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2024-10-03 20:39:43 -0300

    create fixed income - partial

--- src/app/pages/create-fixed-income/create-fixed-income.component.ts
+++ src/app/pages/create-fixed-income/create-fixed-income.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +9,9 @@ import { ItemInfo } from '../../models/item-info.model';
 import { ItemInfoService } from '../../services/item-info.service';
 import { CommonModule } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FixedIncomeService } from '../../services/fixed-income.service';
+import { FixedIncomePostRequest } from '../../models/fixed-income-request.model';
+import { FixedIncome } from '../../models/fixed-income.model';
 
 @Component({
   selector: 'app-create-fixed-income',
@@ -28,13 +31,14 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 export class CreateFixedIncomeComponent implements OnInit {
   private _fb = inject(FormBuilder);
   private _itemInfoService = inject(ItemInfoService);
+  private _fixedIncomeService = inject(FixedIncomeService);
 
   formFixedIncome = this._fb.group({
     description: ['', Validators.required],
     validDate: ['', [Validators.required]],
-    minInvestment: ['', Validators.required],
-    productType: ['', Validators.required],
-    indexer: [{ value: '', disabled: true }, Validators.required],
+    minInvestment: [0, Validators.required],
+    productType: [new ItemInfo(), Validators.required],
+    indexer: [{ value: new ItemInfo(), disabled: true }, Validators.required],
   });
 
   productTypes$: Observable<ItemInfo[]>;
@@ -62,5 +66,23 @@ export class CreateFixedIncomeComponent implements OnInit {
     }
 
     this.indexers$ = this._itemInfoService.getIndexers(productTypeId);
+  }
+
+  create() {
+    const date: Date = this.getControl('validDate').value;
+
+    const request: FixedIncomePostRequest = {
+      descricao: this.getControl('description').value,
+      dataValidade: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+      investimentoMinimo: this.getControl('minInvestment').value,
+      indexadorId: this.getControl('indexer').value.id,
+      tipoProdutoId: this.getControl('productType')?.value?.id,
+    };
+
+    this._fixedIncomeService.post(request).subscribe({
+      next: () => {
+        console.log('success');
+      },
+    });
   }
 }
